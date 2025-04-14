dabc8eeb26791dc3d93f16e9dab028895cbad095
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-14 15:24:19 -0700

    Documentation: Add Docstring for Crossbill Core Package
    This commit adds a docstring comment for the `__init__.py` file in the
    core crossbill source location.

---
+++ src/crossbill/core/__init__.py
@@ -0,0 +1,13 @@
+"""This package contains the Crossbill core package.
+
+Package: crossbill.core
+
+Description:
+Crossbill core provides the abstractual core nouns of crossbill.
+For example:
+* Request
+* Response
+* Service
+* Filter
+* Codec
+"""
