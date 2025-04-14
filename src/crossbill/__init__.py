c88017426c11894717f8c10bfa5e4af11b3e71c6
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-14 15:02:53 -0700

    Documentation: Add Docstring for Crossbill Base Package
    This commit adds a docstring comment for the `__init__.py` file in the
    base crossbill source location.

---
+++ src/crossbill/__init__.py
@@ -0,0 +1,9 @@
+"""This package contains the Crossbill base package.
+
+Package: crossbill
+
+Description:
+Crossbill provides the fundamental building blocks for composing asynchronous
+message-based services centered on the encoding and decoding of domain-specific
+requests and responses to an from bytes.
+"""
