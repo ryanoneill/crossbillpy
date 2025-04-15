d39c6b220923402ea1fce31ae2c11bca24e6c96c
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 16:13:10 -0700

    Transport: Create Transport Package
    This commit puts in place the `__init__.py` file for
    `crossbill.transport`. The `transport` package is responsible for
    shuffling `bytes` across the wire.

---
+++ src/crossbill/transport/__init__.py
@@ -0,0 +1,12 @@
+"""This package contains the Crossbill transport package.
+
+Package: crossbill.transport
+
+Description:
+Crossbill transport provides the components necessary to make bytes go across
+the wire.
+For example:
+* Bridge
+"""
+
+__all__ = []
