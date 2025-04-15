2e8417c2e6d198aa56937311030cef432d1ee446
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-14 17:45:25 -0700

    Core: Add `Service` Abstract Class
    This commit adds the abstract `Service` class. This class is
    foundational building block of `crossbill` and is a relative of, and
    obviously inspired by Finagle's `Service`. At this time, this version
    of `Service` only contains a `__call__` method which is capable of
    asynchronously translating an instance of a `RequestType` into an
    instance of a `ResponseType`.

---
+++ src/crossbill/core/service.py
@@ -0,0 +1,29 @@
+"""Module that includes the `Service` abstract class."""
+
+from abc import abstractmethod
+from typing import Generic
+
+from .request import RequestType
+from .response import ResponseType
+
+class Service(Generic[RequestType, ResponseType]):
+    """A `Service` is an asynchronous function from a `Request` to a `Response`.
+
+    A `Service` transforms an instance of an input type (i.e. `Request`) to an
+    instance of an output type (i.e. `Response`) asynchronously. An instance of
+    a `Service` can be used as an asynchronous function directly by allowing
+    Python to use its `__call__` method. This method within `Service` is abstract
+    and it's expected that concrete implementations of `Service` define it.
+    """
+    @abstractmethod
+    async def __call__(self, request: RequestType) -> ResponseType:
+        """Asynchronous function from `RequestType` to `ResponseType`.
+
+        This method asynchronously returns an instance of a `ResponseType`
+        based on the instance of the `RequestType` passed in. This method
+        can be invoked via the `Service` object itself by using the `Service`
+        object instance as a function. This particular version is abstract.
+        Concrete implementations of `Service` should define it.
+        """
+        pass
+
