3ccea8f3aa79e8b03ec35f8e160d5585ecf2fb8e
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 07:15:43 -0700

    Test: Refactor String-Based Test Utilities to Own File
    This commit moves the string-based testing utilities (`StringRequest`,
    `StringResponse`, and `EchoService`) out of `tests/test_service.py` and
    into `tests/strings.py` so that they may be used by additional tests.

---
+++ tests/strings.py
@@ -0,0 +1,18 @@
+from crossbill.core import Request, Response, Service
+
+
+class StringRequest(Request):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+
+class StringResponse(Response):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+
+class EchoService(Service[StringRequest, StringResponse]):
+    async def __call__(self, request: StringRequest) -> StringResponse:
+        return StringResponse(request.value)
+
+
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,22 +1,6 @@
 import pytest
 
-from crossbill.core import Request, Response, Service
-
-
-class StringRequest(Request):
-    def __init__(self, value: str) -> None:
-        self.value = value
-
-
-class StringResponse(Response):
-    def __init__(self, value: str) -> None:
-        self.value = value
-
-
-class EchoService(Service[StringRequest, StringResponse]):
-    async def __call__(self, request: StringRequest) -> StringResponse:
-        return StringResponse(request.value)
-
+from strings import StringRequest, EchoService
 
 @pytest.mark.anyio
 async def test_echo():
