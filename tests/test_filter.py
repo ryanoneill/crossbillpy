a931c0bed55a1530fdb7c806f5f3da2c4e99f06d
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 07:18:22 -0700

    Test: Add First `Filter` Test
    This commit adds the first unit test for the `Filter` class.

---
+++ tests/test_filter.py
@@ -0,0 +1,31 @@
+import pytest
+
+from crossbill.core import Filter, Service
+
+from strings import StringRequest, StringResponse, EchoService
+
+class ReverseResponseFilter(Filter[StringRequest, StringResponse]):
+    async def __call__(self, request: StringRequest, service: Service[StringRequest, StringResponse]) -> StringResponse:
+        response = await service(request)
+        response.value = response.value[::-1]
+        return response
+
+@pytest.mark.anyio
+async def test_reverse_response():
+    filter = ReverseResponseFilter()
+
+    # Non-filtered
+    service = EchoService()
+    request = StringRequest("hello")
+    response = await service(request)
+    result = response.value
+    assert result == "hello"
+
+    # Filtered
+    request = StringRequest("hello")
+    response = await filter(request, service)
+    result = response.value
+    assert result == "olleh"
+
+
+
