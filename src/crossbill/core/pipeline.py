d95d882e3ec1e1610cdcd42eee150be382c09576
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 15:15:51 -0700

    Core: Add `Pipeline` Class
    A `Pipeline` is intended for use in building a server, where `bytes` are
    processed, converted to a `Request`, that `Request` is processed by a
    `Service` which returns a `Response`, and then that `Response` is
    converted back into `bytes`. A `Pipeline` is responsible for performing
    those transformations given the correct pieces.

---
+++ src/crossbill/core/pipeline.py
@@ -0,0 +1,37 @@
+"""Module that includes the `Pipeline` class."""
+
+from typing import Generic
+
+from .request import RequestType, RequestCodec
+from .response import ResponseType, ResponseCodec
+from .service import Service
+
+
+class Pipeline(Generic[RequestType, ResponseType]):
+    """A `Pipeline` asynchronous takes input `bytes` and returns output `bytes`.
+
+    A `Pipeline` follows a path of `bytes` -> `Request` -> `Service` ->
+    `Response` -> `bytes`. This is done by using a `RequestCodec` to convert
+    from `bytes` to a `Request`. Then the `Request` is turned into a `Response`
+    by the `Service`. Next the `Response` is turned back into `bytes` by a
+    `ResponseCodec` and then those `bytes` are returned by the `Pipeline`.
+    """
+
+    def __init__(
+        self,
+        request_codec: RequestCodec[RequestType],
+        response_codec: ResponseCodec[ResponseType],
+        service: Service[RequestType, ResponseType],
+    ) -> None:
+        """Initialize a `Pipeline` using `Codec`s and a `Service`."""
+        self.request_codec = request_codec
+        self.response_codec = response_codec
+        self.service = service
+
+    async def __call__(self, data: bytes) -> bytes:
+        """Asynchronously return output `bytes` based on input `bytes`."""
+        request = await self.request_codec.decode(data)
+        response = await self.service(request)
+        result = await self.response_codec.encode(response)
+
+        return result
