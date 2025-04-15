71a23a89b0c2f1b31f4f9b6ee088fca441c32111
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 08:52:40 -0700

    Test: Add a Test for the `IdentityCodec`
    This commit adds the first (and likely) only unit test for the
    `IdentityCodec`. It's there to flex the API and ensure that data that's
    passed in comes back untouched.

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -1,6 +1,7 @@
 import pytest
 
 from crossbill.core import Codec
+from crossbill.core.codec import IdentityCodec
 
 class StringCodec(Codec[str, bytes]):
     async def encode(self, data: str) -> bytes:
@@ -25,3 +26,15 @@ async def test_codec_decode_encode() -> None:
     string = await codec.decode(before)
     after = await codec.encode(string)
     assert before == after
+
+@pytest.mark.anyio
+async def test_identity_codec() -> None:
+    codec = IdentityCodec()
+    value = "Hello World"
+    encoded = await codec.encode(value)
+    assert value == encoded
+    decoded = await codec.decode(value)
+    assert value == decoded
+
+    # Ensure the initial value is untouched
+    assert value == "Hello World"
