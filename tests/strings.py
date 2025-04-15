61ec3ba4668c52c13b62cb1cede0997fd9f95cb0
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 09:41:24 -0700

    Test: Add `StringRequestCodec` and `StringResponseCodec`
    This commit adds the `StringRequestCodec` and `StringResponseCodec`
    classes, which can transform bytes to and from `StringRequest`s and
    `StringResponse`s respectively.

--- tests/strings.py
+++ tests/strings.py
@@ -1,4 +1,4 @@
-from crossbill.core import Request, Response, Service
+from crossbill.core import Request, RequestCodec, Response, ResponseCodec, Service
 
 
 class StringRequest(Request):
@@ -9,6 +9,22 @@ class StringRequest(Request):
 class StringResponse(Response):
     def __init__(self, value: str) -> None:
         self.value = value
+
+
+class StringRequestCodec(RequestCodec[StringRequest]):
+    async def encode(self, data: StringRequest) -> bytes:
+        return data.value.encode()
+
+    async def decode(self, data: bytes) -> StringRequest:
+        return StringRequest(data.decode())
+
+
+class StringResponseCodec(ResponseCodec[StringResponse]):
+    async def encode(self, data: StringResponse) -> bytes:
+        return data.value.encode()
+
+    async def decode(self, data: bytes) -> StringResponse:
+        return StringResponse(data.decode())
 
 
 class EchoService(Service[StringRequest, StringResponse]):
