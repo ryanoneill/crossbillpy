505c288d1f17a9042e5d31dcca5591426d1a265a
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-14 18:35:38 -0700

    Test: Add Echo Test for Service
    This commit adds our first unit test using a `StringRequest`, a
    `StringResponse` and an `EchoService`, and ensures that the value
    returned is the value passed in.

---
+++ tests/test_service.py
@@ -0,0 +1,23 @@
+import pytest
+
+from crossbill.core import Request, Response, Service
+
+class StringRequest(Request):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+class StringResponse(Response):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+class EchoService(Service[StringRequest, StringResponse]):
+    async def __call__(self, request: StringRequest) -> StringResponse:
+        return StringResponse(request.value)
+
+@pytest.mark.anyio
+async def test_echo():
+    service = EchoService()
+    request = StringRequest("hello")
+    response = await service(request)
+    result = response.value
+    assert result == "hello"
