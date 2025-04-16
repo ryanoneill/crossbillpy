259628878e94f693de38836907e7b6092643c7ae
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-15 22:42:44 -0700

    Transport: Add `Bridge` Class
    This commit adds the `Bridge` class to the `crossbill.transport`
    package. A `Bridge` is intended to be the place that shuffles `bytes` to
    and from `asyncio` for a `Pipeline`.

---
+++ src/crossbill/transport/bridge.py
@@ -0,0 +1,20 @@
+"""Module that includes the `Bridge` class."""
+
+from asyncio import StreamReader, StreamWriter
+from typing import Generic
+
+from ..core import Pipeline, RequestType, ResponseType
+
+
+class Bridge(Generic[RequestType, ResponseType]):
+    """A `Bridge` is shuffles `bytes` to and from an underlying transport."""
+
+    def __init__(self, pipeline: Pipeline) -> None:
+        """Initialize a `Bridge` by supplying a `Pipeline`."""
+        self.pipeline = pipeline
+
+    async def __call__(self, reader: StreamReader, writer: StreamWriter) -> None:
+        """Asynchronously shuffle `bytes` from a `StreamReader`/`StreamWriter`."""
+        while in_data := await reader.read(1024):
+            out_data = await self.pipeline(in_data)
+            writer.write(out_data)
