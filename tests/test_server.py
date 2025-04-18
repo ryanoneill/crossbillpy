bc069a0091eee55cfb016f8461a393dd74dbe436
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-18 12:25:21 -0700

    Test: Improve `Server` Test Coverage
    This commit adds tests around `is_running` for `Server` that bring the
    overall test coverage back up to 100%.

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,26 @@
 import pytest
 
+from strings import EchoService
+
 from crossbill.string import StringServer
+from crossbill.transport import Address
+
+
+def test_is_running_on_empty() -> None:
+    server = StringServer()
+
+    result = server.is_running()
+    assert not result
+
+
+@pytest.mark.asyncio
+async def test_is_running_on_running() -> None:
+    server = StringServer()
+    address = Address("localhost", 10555)
+    await server.serve(address, EchoService())
+    assert server.is_running()
+    await server.close()
+    assert not server.is_running()
 
 
 @pytest.mark.asyncio
@@ -9,3 +29,5 @@ async def test_close_on_empty() -> None:
 
     # Nothing bad should happen here
     await server.close()
+
+
