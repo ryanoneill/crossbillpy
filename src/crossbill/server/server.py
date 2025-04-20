8876145202318fa32777cdc4366151a52b20d852
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-20 10:14:36 -0700

    Server: Add `serve_forever` Method to `Server`
    This commit modifies the `Server` lifecycle a bit to add a
    `serve_forever` method that will essentially block until the `Server` is
    shut down.

--- src/crossbill/server/server.py
+++ src/crossbill/server/server.py
@@ -2,6 +2,7 @@
 
 import asyncio
 from asyncio import Server as AsyncioServer
+from asyncio.exceptions import CancelledError
 from typing import Optional
 
 from ..core import Closable, PipelineFactory, ReqRepType, Service
@@ -19,6 +20,7 @@ class Server(Closable, ReqRepType):
         """
         self.pipeline_factory = pipeline_factory
         self.server: Optional[AsyncioServer] = None
+        self._run_task = None
 
     def is_running(self) -> bool:
         """Returns `True` if the `Server` is running, `False` otherwise."""
@@ -28,23 +30,41 @@ class Server(Closable, ReqRepType):
         return result
 
     async def _run(self, server: AsyncioServer) -> None:
-        async with server:
-            await server.serve_forever()
+        try:
+            async with server:
+                await server.serve_forever()
+        except CancelledError:
+            pass
+
+
+    async def _setup(self, address: Address, service: Service) -> AsyncioServer:
+        pipline = await self.pipeline_factory(service)
+        bridge = Bridge(pipline)
+        return await asyncio.start_server(bridge, address.host, address.port)
 
     async def serve(self, address: Address, service: Service) -> None:
         """Start listening on the given `Address` and pass requests to `Service`."""
-        pipline = await self.pipeline_factory(service)
-        bridge = Bridge(pipline)
-        self.server = await asyncio.start_server(bridge, address.host, address.port)
+        self.server = await self._setup(address, service)
         # Don't assign the created task to '_'. That's a mistake
         # Doing so allows the background run task to be collected, which
         # is undesirable.
         self._run_task = asyncio.create_task(self._run(self.server))
         await self.server.start_serving()
 
+    async def serve_forever(self, address: Address, service: Service) -> None:
+        """Start listening on the given `Address` and pass requests to `Service`.
+
+        This method differs from `serve` in that the `Coroutine` returned will
+        not be done until the `Server` shuts down.
+        """
+        self.server = await self._setup(address, service)
+        await self._run(self.server)
+
     async def close(self) -> None:
         """Close a `Server`. If not running, that's ok."""
         if self.server:
+            if self._run_task:
+                self._run_task.cancel("Closing")
             self.server.close()
             await self.server.wait_closed()
             self.server = None
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,8 @@
+import asyncio
 import pytest
 from strings import EchoService
 
+from crossbill.server import Server
 from crossbill.string import StringServer
 from crossbill.transport import Address
 
@@ -28,3 +30,21 @@ async def test_close_on_empty() -> None:
 
     # Nothing bad should happen here
     await server.close()
+
+
+@pytest.mark.asyncio
+async def test_serve_forever() -> None:
+    server = StringServer()
+    address = Address("localhost", 10555)
+    task = asyncio.create_task(close_serve_forever(server))
+    await server.serve_forever(address, EchoService())
+    await task
+
+
+async def close_serve_forever(server: Server) -> None:
+    while not server.is_running():
+        await asyncio.sleep(0.1)
+    assert server.is_running()
+    await server.close()
+    assert not server.is_running()
+
