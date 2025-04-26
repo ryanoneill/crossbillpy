dda7dd23a57782b4f3b1d68bf0df0ee80ba657e8
Ryan O'Neill <ryanoneill@gmail.com>
2025-04-26 15:35:18 -0700

    Http: Add Beginnings of `http` for `crossbill`
    This commit adds the `__init__.py` file for `crossbill.http`. It
    currently contains no code.

---
+++ src/crossbill/http/__init__.py
@@ -0,0 +1,10 @@
+"""This package contains the Crossbill http package.
+
+Package: crossbill.http
+
+Description:
+Crossbill http provides the components necessary to create clients and servers
+that operate on http requests and responses.
+"""
+
+__all__ = []
