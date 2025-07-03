42fcffa1c4eef42c17bc7d25c526d1f42fd4ee38
Ryan O'Neill <ryanoneill@gmail.com>
2025-07-03 15:13:21 -0700

    Redis: Introduce Redis Package
    This commit holds the beginnings of `crossbill.redis`. It is only the
    `__init__.py` file at this time.

---
+++ src/crossbill/redis/__init__.py
@@ -0,0 +1,10 @@
+"""This package contains the Crossbill redis package.
+
+Package: crossbill.redis
+
+Description:
+Crossbill redis provides the components necessary to create clients and servers
+that operate on redis commands and replies.
+"""
+
+__all__ = []
