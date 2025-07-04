41518806371db4156e39b7a97cc54fa45116a78a
Ryan O'Neill <ryanoneill@gmail.com>
2025-07-04 13:49:08 -0700

    Redis: Add Tests for Empty Bulk String Encode / Decode
    This commit adds basic tests for the encoding and decoding of empty
    strings as Redis bulk strings:
    
    * ""
    * b"$0\r\n\r\n"
    
    This additional check is to ensure that the `BulkStringCodec` works
    as expected.

--- tests/test_bulk_string_codec.py
+++ tests/test_bulk_string_codec.py
@@ -10,3 +10,13 @@ async def test_bulk_string_codec_decode() -> None:
     codec = BulkStringCodec()
     result = await codec.decode(b"$5\r\nhello\r\n")
     assert result == "hello"
+
+async def test_empty_bulk_string_codec_encode() -> None:
+    codec = BulkStringCodec()
+    result = await codec.encode("")
+    assert result == b"$0\r\n\r\n"
+
+async def test_empty_bulk_string_codec_decode() -> None:
+    codec = BulkStringCodec()
+    result = await codec.decode(b"$0\r\n\r\n")
+    assert result == ""
